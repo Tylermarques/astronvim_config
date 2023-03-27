cef52ee7cf3355efd7784bee7b66e6d8540b55c3
Tyler Marques <me@tylermarques.com>
2023-03-27 14:00:50 -0400

    Moved to top level
    Signed-off-by: Tyler Marques <me@tylermarques.com>

---
+++ lua/user/plugins/user.lua
@@ -0,0 +1,27 @@
+return {
+  -- You can also add new plugins here as well:
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({})
+    end,
+  },
+  {
+    "zbirenbaum/copilot-cmp",
+    after = { "copilot.lua" },
+    config = function()
+      require("copilot_cmp").setup()
+    end
+  },
+  -- Add plugins, the lazy syntax
+  -- "andweeb/presence.nvim",
+  -- {
+  --   "ray-x/lsp_signature.nvim",
+  --   event = "BufRead",
+  --   config = function()
+  --     require("lsp_signature").setup()
+  --   end,
+  -- },
+}

07543dcd887a2087cb2ebdbe56581b271bcdf4a7
Tyler Marques <me@tylermarques.com>
2024-04-17 10:10:33 -0400

    Removing format on save, was creating huge git diffs, making me look like an ass hole for formatting everyone's stuff
    Signed-off-by: Tyler Marques <me@tylermarques.com>

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -48,7 +48,7 @@ return {
     formatting = {
       -- control auto formatting on save
       format_on_save = {
-        enabled = true, -- enable or disable format on save globally
+        enabled = false, -- enable or disable format on save globally
       },
       timeout_ms = 1000, -- default format timeout
     },
